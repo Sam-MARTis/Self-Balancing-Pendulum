b7c0d5f4055ae6104c9bab2ce2b23c311d9bc212
Sam-MARTis <samanthmartis@gmail.com>
2025-07-27 19:09:51 +0530

    Added insertiona dn reset functions + mutate

--- Python-Version/agent.py
+++ Python-Version/agent.py
@@ -1,6 +1,5 @@
 from utils import Helpers
-
-
+from typing import List
 class Neuron:
     def __init__(self, id=0, value=0.0, activation_function=lambda x: x):
         self.id = id
@@ -14,38 +13,54 @@ class Gene:
         self.bias = bias
         
 class Brain:
-    def __init__(self, id=0, neurons=None, genes=None):
+    def __init__(self, id:int=0, neurons:List[Neuron]=None, genes:List[Gene]=None):
         if neurons is None:
-            neurons = []
+            neurons:List[Neuron] = []
         if genes is None:
-            genes = []
+            genes:List[Gene] = []
         self.id = id
         self.neurons = neurons
         self.genes = genes
-
+    def reset_neurons(self):
+        for neuron in self.neurons:
+            neuron.value = 0.0
+    
+    
     def perform_computation(self, state):
         pass
+    
 
     def crossover(self, other):
         pass
+    
+    def insert_neuron(self, neuron:Neuron, position:int):
+        if position < 0 or position > len(self.neurons):
+            raise IndexError("Position out of bounds")
+        self.neurons.insert(position, neuron)
+    
+    def insert_gene(self, gene:Gene, position:int):
+        if position < 0 or position > len(self.genes):
+            raise IndexError("Position out of bounds")
+        self.genes.insert(position, gene)
+        
 
     def mutate(self, mutation_rate, mutation_strength_weight=0.1, mutation_strength_bias=0.1):
-        for gene in self.genes:
-            if Helpers.rand(0, 1) < mutation_rate:
+        for gene in self.genes: #Replace with tower based sampling
+            if Helpers.rand(0, 1) < mutation_rate: 
                 gene.weight += Helpers.rand(-mutation_strength_weight, mutation_strength_weight)
                 gene.bias += Helpers.rand(-mutation_strength_bias, mutation_strength_bias)
         pass
     
-    def save_brain(self, filename):
+    def save_brain(self, filename:str):
         pass
 
 
 class Generation:
     def __init__(self, brains_count=1, input_nodes = 4, outut_nodes = 1):
-        self.brains = []
+        self.brains:List[Brain] = []
         for i in range(brains_count):
-            neurons = [Neuron(id=j) for j in range(input_nodes + outut_nodes)]
-            genes = [Gene(from_node_id=j, to_node_id=(j + 1) % (input_nodes + outut_nodes), weight=1.0, bias=0.0) for j in range(input_nodes)]
+            neurons:List[Neuron] = [Neuron(id=j) for j in range(input_nodes + outut_nodes)]
+            genes:List[Neuron] = [Gene(from_node_id=j, to_node_id=(j + 1) % (input_nodes + outut_nodes), weight=1.0, bias=0.0) for j in range(input_nodes)]
             self.brains.append(Brain(id=i, neurons=neurons, genes=genes))
             
     def evaluate_agents(self, fitnessfunction):
