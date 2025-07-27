08fbb4e7df409ae2f00feb742f73f791132fe5c1
Sam-MARTis <samanthmartis@gmail.com>
2025-07-27 12:43:29 +0530

    Improved python engine

--- app.py
+++ app.py
@@ -3,59 +3,36 @@ import math
 import sys
 
 # Pygame Setup
-pygame.init()
+
+
+
 WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pendulum on Cart")
-clock = pygame.time.Clock()
-
-
-cart_width = 0.5
-cart_height = 0.2
-pendulum_length = 0.5
-pendulum_angle = math.radians(30)
-
 ARENA_WIDTH = 3
-magnification = WIDTH / ARENA_WIDTH
+CART_WIDTH = 0.3
+CART_HEIGHT = 0.1
+CART_MASS = 1.0
+PENDULUM_LENGTH = 0.3
 
+PENDULUM_MASS = 1.0
+PENDULUM_THICKNESS = 0.02
+PENDULUM_BOB_RADIUS = 0.03
+
+
+
+CART_MAX_SPEED = 5
+MAX_ACCELERATION = 40
+CART_BUTTON_FORCE = 8
+SWING_FRICTION_CONSTANT = 0.1
+GRAVITY = 9.81
+LATERAL_FRICTION_CONSTANT = 0.1
+
+
+pendulum_angle = math.radians(30)
+magnification = WIDTH / ARENA_WIDTH
 cart_x = ARENA_WIDTH/2
 cart_y =  HEIGHT / (2 * magnification)
-cart_max_speed = 0.1
-SWING_FRICTION_CONSTANT = 0.1
-# Main Loop
-
-"""
 
 
-void Cart::accelerate(const float force, const float gravity = GRAVITY)
-{
-    // const float acceleration = (force - aPend.mass * gravity * sin(aPend.angle) * cos(aPend.angle)) / (mass + aPend.mass * SQR(cos(aPend.angle)));
-    const float acceleration = (force/mass)  - LATERAL_FRICTION_CONSTANT*velocity;
-    this->acceleration = CLAMP(acceleration, -max_acceleration, max_acceleration);
-}
-void Cart::step(const float dt, const float force, const float gravity = GRAVITY)
-{
-    accelerate(force, gravity);
-    velocity += (acceleration) * dt ;
-    velocity = CLAMP(velocity, -max_speed, max_speed);
-    position += velocity * dt;
-    const vec2 pendForce = {-aPend.mass * acceleration, -aPend.mass * gravity};
-    aPend.step(dt, pendForce);
-}
-
-void Pendulum::step(float dt, vec2 force)
-{
-    const float torque = ((-force.x * length * sin(angle)) +
-                         (force.y * length * cos(angle))) - SWING_FRICTION_CONSTANT * angular_velocity;
-    const float angular_acceleration = torque * MOI_inv;
-    angular_velocity += angular_acceleration * dt;
-    angle += angular_velocity * dt;
-}
-
-
-"""
-
-MAX_ACCELERATION = 40
 
 
 class vec2:
@@ -76,7 +53,8 @@ class vec2:
         return vec2(self.x / scalar, self.y / scalar)
 
 class Cart:
-    def __init__(self, screen, width, height):
+    def __init__(self, screen, mass, width, height):
+        self.mass = mass
         self.screen = screen
         self.width = width
         self.height = height
@@ -85,17 +63,22 @@ class Cart:
         self.accx = 0
         self.pendulum: Pendulum = None
         
-    def accelerate(self, force, gravity=9.81):
+    def accelerate(self, force):
         # Calculate acceleration based on force and gravity
-        acceleration = (force / self.width) - SWING_FRICTION_CONSTANT * self.vx
+        acceleration = (force/ self.mass)
         self.accx = max(-MAX_ACCELERATION, min(MAX_ACCELERATION, acceleration))
     
         
     def step(self, dt):
-        self.vx += self.accx * dt
-        # self.vx = max(-cart_max_speed, min(cart_max_speed, self.vx))
+        oldvx = self.vx
+        vx = self.vx +  (self.accx - (LATERAL_FRICTION_CONSTANT * self.vx) )*dt
+        self.vx = max(-CART_MAX_SPEED, min(CART_MAX_SPEED, vx))
+        accx = (self.vx - oldvx) / dt
+        pendulum_force = vec2(-self.pendulum.mass * accx, -self.pendulum.mass * GRAVITY)
+        
+        # self.vx -= * dt
         self.x += self.vx * dt
-        pendulum_force = vec2(-self.pendulum.mass * self.accx, -self.pendulum.mass * 9.81)
+        
         self.accx = 0
         self.pendulum.update(dt, pendulum_force)
     
@@ -133,18 +116,29 @@ class Pendulum:
         pendulum_x = pivot_x + self.length * math.cos(self.angle)
         pendulum_y = pivot_y - self.length * math.sin(self.angle)
 
-        pygame.draw.line(self.screen, (255, 255, 255), (pivot_x*magnification, pivot_y*magnification), (pendulum_x*magnification, pendulum_y*magnification), 10)
-        pygame.draw.circle(self.screen, (255, 0, 0), (int(pendulum_x*magnification), int(pendulum_y*magnification)), 12)
+        pygame.draw.line(self.screen, (255, 255, 255), (pivot_x*magnification, pivot_y*magnification), (pendulum_x*magnification, pendulum_y*magnification), math.ceil(PENDULUM_THICKNESS*magnification))
+        pygame.draw.circle(self.screen, (255, 0, 0), (int(pendulum_x*magnification), int(pendulum_y*magnification)), math.ceil(PENDULUM_BOB_RADIUS*magnification))
+
     
 
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    cart = Cart(screen, cart_width, cart_height)
-    pendulum = Pendulum(screen, cart, pendulum_length, 1.0, pendulum_angle)
+    pygame.init()
+    pygame.display.set_caption("Pendulum on Cart")
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    cart = Cart(screen, CART_MASS, CART_WIDTH, CART_HEIGHT)
+    pendulum = Pendulum(screen, cart, PENDULUM_LENGTH, PENDULUM_MASS, pendulum_angle)
     cart.pendulum = pendulum
 
     # Main loop
     clock = pygame.time.Clock()
-    cart_speed = 5
     pendulum.angular_velocity = 0.0
     while True:
         screen.fill((0, 0, 0))  
@@ -156,9 +150,9 @@ if __name__ == "__main__":
         keys = pygame.key.get_pressed()
         force = 0
         if keys[pygame.K_LEFT]:
-            force = -cart_speed
+            force = -CART_BUTTON_FORCE
         elif keys[pygame.K_RIGHT]:
-            force = cart_speed
+            force = CART_BUTTON_FORCE
 
         cart.accelerate(force)
         dt = clock.tick(60)/1000
